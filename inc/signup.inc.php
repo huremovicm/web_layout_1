dea820cc94db7398de27924e4072b4480dc1ded8
Maid Huremovic <maid.huremovic@outlook.com>
2020-07-17 12:12:50 +0200

    contact form connected with db

--- inc/signup.inc.php
+++ inc/signup.inc.php
@@ -6,9 +6,11 @@ if (isset($_POST['submit'])) {
 $first_name = $_POST['firstname'];
 $last_name = $_POST['lastname'];
 $age = $_POST['age'];
+$country = $_POST['country'];
+$short_message = $_POST['subject'];
 
 
-$sql = "INSERT  INTO users (first_name, second_name, age) VALUES ('$first_name', '$last_name', '$age'); ";
+$sql = "INSERT  INTO users (first_name, second_name, age, country, short_message) VALUES ('$first_name', '$last_name', '$age', '$country', '$short_message'); ";
 $result = mysqli_query($conn, $sql);
 
 header("Location: ../index.php?signup=success");
